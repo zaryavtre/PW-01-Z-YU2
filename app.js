aaae3497172dc1841a6ca6117b88fd531295a207
Stéphane <von.zaryavtre@gmail.com>
2023-02-16 19:45:52 +0000

    Random Numbers working

--- app.js
+++ app.js
@@ -1,28 +1,115 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+];
 
-let n = 5
+let n = 5;
 
-const btn = document.querySelector('.btn')
-const pwBoxOne = document.querySelector('#p-box-one')
-const pwBoxTwo = document.querySelector('#pwbox2')
-
+const btn = document.querySelector('.btn');
+const pwBoxOne = document.querySelector('#p-box-one');
+const pwBoxTwo = document.querySelector('#pwbox2');
 
 function pushPw() {
-    for(let i = 0; i < n; i++){
-        let randomIndex = Math.floor(Math.random() * characters.length)
-        return randomIndex[characters]
-    }
+  for (let i = 0; i < n; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    return randomIndex[characters];
+  }
 }
 
-pushPw()
+pushPw();
 
-
-btn.addEventListener('click', function() {
-        let randomPW = pushPw()
-        pwBoxOne.textContent = randomPW
-
-    
-})
+btn.addEventListener('click', function () {
+  let randomPW = pushPw();
+  pwBoxOne.textContent = randomPW;
+});
 
 // characters[Math.floor(Math.random() * characters.length)]
