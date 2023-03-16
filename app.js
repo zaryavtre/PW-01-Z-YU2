5cd48ef16e6f2c40a293782a645ac434cee712f8
Stéphane <von.zaryavtre@gmail.com>
2023-03-16 17:26:55 +0000

    Added Character Array in another folder

--- app.js
+++ app.js
@@ -1,96 +1,4 @@
-const characters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '~',
-  '`',
-  '!',
-  '@',
-  '#',
-  '$',
-  '%',
-  '^',
-  '&',
-  '*',
-  '(',
-  ')',
-  '_',
-  '-',
-  '+',
-  '=',
-  '{',
-  '[',
-  '}',
-  ']',
-  ',',
-  '|',
-  ':',
-  ';',
-  '<',
-  '>',
-  '.',
-  '?',
-  '/',
-]
+import { characters } from '/chArray.js'
 
 let RandomNumbers = []
 let boxNumbers2 = []
@@ -114,11 +22,11 @@ function pushPw() {
 }
 
 btn.addEventListener('click', function () {
-    pushPw()
-    pwBoxOne.textContent = RandomNumbers.join('')
-    pwBoxTwo.textContent = boxNumbers2.join('')
-    RandomNumbers = []
-    boxNumbers2 = []
+  pushPw()
+  pwBoxOne.textContent = RandomNumbers.join('')
+  pwBoxTwo.textContent = boxNumbers2.join('')
+  RandomNumbers = []
+  boxNumbers2 = []
 })
 
 // characters[Math.floor(Math.random() * characters.length)]
